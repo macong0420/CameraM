1effdca8bdf7a5a1dea0abb50735b8e2017c82f4
马聪聪 <macong0420@126.com>
2025-09-18 15:48:24 +0800

    Initial Commit

---
+++ CameraM/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  CameraM
+//
+//  Created by 马聪聪 on 2025/9/18.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ CameraM/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  CameraM
+//
+//  Created by 马聪聪 on 2025/9/18.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
